5b53124e0a0ac3c4d5ed7141095ac527f8fd1291
jzhousz <jzhousz@56dcbd0c-88c3-11dd-ba2e-09f2ff3e973f>
2011-09-03 00:27:31 +0000

    Updated to accept ArrayList instead of byte[][]

--- trunk/iano/annotool/extract/FeatureExtractor.java
+++ trunk/iano/annotool/extract/FeatureExtractor.java
@@ -1,6 +1,7 @@
 package annotool.extract;
 
 import annotool.io.DataInput;
+import java.util.ArrayList;
 import annotool.ImgDimension;
 
 /** interface for all feature extractors such as HaarFeatureExtractor **/
@@ -11,17 +12,16 @@ public interface FeatureExtractor
    public void  setParameters(java.util.HashMap<String, String> parameter);
    
    //get features based on raw image in the problem
-   public float[][] calcFeatures(DataInput problem);
+   public float[][] calcFeatures(DataInput problem) throws Exception;
 
-   //get features based on byte data, with dimension information. 
-   public float[][] calcFeatures(byte[][] data, ImgDimension dim);
+   //get features based on data, with dimension information. 
+   //public float[][] calcFeatures(byte[][] data, ImgDimension dim);
+   //ArrayList of array can accommodate FloatProcessor and IntProcessor 09/02/2011
+   public float[][] calcFeatures(ArrayList data, int imageType, ImgDimension dim) throws Exception;
 
    //indicate if it is applicable to 3D image stack.
    boolean is3DExtractor();
 
-   //get features based on input data in the instance	
-   //public float[][] calcFeatures();
-   
    //get features based on input data. Not supported by all feature extractors especially 3D.
    //public float[][] calcFeatures(float[][] data, DataInput problem);
 
