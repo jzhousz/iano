83528597fe103895ce067dea68d0604e25d57c87
lukechalland@gmail.com <lukechalland@gmail.com@56dcbd0c-88c3-11dd-ba2e-09f2ff3e973f>
2012-08-24 20:02:05 +0000

    Updated the dates and version numbers of BioCat.

--- trunk/iano/annotool/gui/AnnMenuBar.java
+++ trunk/iano/annotool/gui/AnnMenuBar.java
@@ -319,9 +319,9 @@ public class AnnMenuBar implements ActionListener {
 	public void displayAbout()
 	{
 		String aboutString =  "BioCAT (Biological Image Classification and Annotation Tool)\n\n" +
-		"Version: 1.0.0\n\n" +
+		"Version: 1.2.0\n\n" +
 		"Jie Zhou, Hanchuan Peng, Santosh Lamichhane and other contributors\n" +
-		"2009 - 2011.  All rights reserved.\n\n" +
+		"2009 - 2012.  All rights reserved.\n\n" +
 		"This tool uses software below:\n" +
 		"mRMR -- \thttp://research.janelia.org/peng/proj/mRMR/ \n" +
 		"ImageJ -- \thttp://rsbweb.nih.gov/ij \n" +
