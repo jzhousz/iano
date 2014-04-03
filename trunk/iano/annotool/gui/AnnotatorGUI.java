481ed0a6c03a8befb57f3a9d3f12b4ecab55558d
jzhousz@gmail.com <jzhousz@gmail.com@56dcbd0c-88c3-11dd-ba2e-09f2ff3e973f>
2014-04-03 02:29:35 +0000

    update to release 1.3 in GUI and About

--- trunk/iano/annotool/gui/AnnotatorGUI.java
+++ trunk/iano/annotool/gui/AnnotatorGUI.java
@@ -71,7 +71,7 @@ public class AnnotatorGUI extends JFrame
 	        	  System.out.println("Substance L&F failed to initialize");
 	          }
     		
-	          AnnotatorGUI gui = new AnnotatorGUI("BioCAT -- Biological Image Classification and Annotation Tool 1.2");
+	          AnnotatorGUI gui = new AnnotatorGUI("BioCAT -- Biological Image Classification and Annotation Tool 1.3");
 	          System.out.println("Hello BioCAT!");
     		
 	          gui.pack();
