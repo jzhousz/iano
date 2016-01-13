364d62c8e189d7e81f6dc66e5c7ba6a1e428f3e8
JonSanders890 <Sandahz09@gmail.com>
2016-01-13 12:37:29 -0600

    proper override of equals() and added a hashing function (needed when using equals()! )

--- SynapseCounter_TopoMap/ThreeDSynapseCounter/Point3D.java
+++ SynapseCounter_TopoMap/ThreeDSynapseCounter/Point3D.java
@@ -3,20 +3,32 @@
 class Point3D
 {
 	int x, y, z;
+	
+	//constructor
 	Point3D(int x, int y, int z) {this.x=x; this.y=y; this.z=z;}
-	//Return tree if 2 points are close enough
-	boolean equals(Point3D p2)
+	
+	//Return true if 2 points are close enough
+	@Override
+	public boolean equals(Object p2)
 	{ 
 		int distx, disty, distz;
-		if (x>p2.x) distx = x-p2.x; else distx = p2.x -x;
+		//this is super misleading, doesnt equate points at all! -Jon Sanders 12/15
+		/*if (x>p2.x) distx = x-p2.x; else distx = p2.x -x;
 		if (y>p2.y) disty = y-p2.y; else disty = p2.y -y;
 		if (z>p2.z) distz = z-p2.z; else distz = p2.z -z;
 		//distance on xy plane should be smaller than 2, on z should be smaller than 1. - Due to different resolution, don't say distx+disty+distz < 3
 		if((distx<2) && (disty<=2) && distz <=1) return true;
-		else return false; 
+		*/
+		if(p2 instanceof Point3D ) {
+			Point3D p = (Point3D) p2;
+			if( (x == p.x) && (y == p.y) && (z == p.z)) return true;
+		}
+		
+		return false; 
 	}
 
-	boolean closesTo(Point3D p2, int dx, int dy, int dz)
+	//neighborhood check to see if a point is within a region near this point
+	public boolean closesTo(Point3D p2, int dx, int dy, int dz)
 	{ 
 		int distx, disty, distz;
 		if (x>p2.x) distx = x-p2.x; else distx = p2.x -x;
@@ -27,16 +39,22 @@ class Point3D
 		else return false; 
 	}
 
+	//return the point as a string 'x,y,z'
 	String convertToString() {
 	
-		return ("("+x+","+y+","+z+")");
+		return (""+x+","+y+","+z);
 	
 	}
 	
-	void offsetPoint(int offX, int offY, int offZ) {
-		x += offX;
-		y += offY;
-		z += offZ;
+	//custom hash NEEDED when override equals
+	@Override
+	public int hashCode() {
+		int hash = 17;
+		hash = hash*31 + z;
+		hash = hash*13 + y;
+		hash = hash*23 + x;
+		return hash;
 	}
+	
 }
 
